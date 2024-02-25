b731c544cc12244f89fb669455864e8e23854f3b
RafaelMatos <rafael-matos@outlook.com>
2024-02-24 22:54:55 -0300

    style: change BoxHeader padding

--- src/components/BoxHeader/styles.ts
+++ src/components/BoxHeader/styles.ts
@@ -2,7 +2,7 @@ import { styled } from '../../../stitches.config'
 
 export const Container = styled('div', {
   borderBottom: '1px solid $gray700',
-  padding: '$4',
+  padding: '0 $2 $4',
   width: '100%',
   marginBottom: '$4',
 })
